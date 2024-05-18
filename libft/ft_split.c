b11476a70d747dbef5626506a32e1c8b6fd580c9
PatAngu8 <123322863+PatAngu8@users.noreply.github.com>
2024-05-18 22:47:16 +0200

    split ok.
    reajustar substr que le sobran líneas

---
+++ libft/ft_split.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patri <patriciaangulo.g@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 21:47:29 by patri             #+#    #+#             */
+/*   Updated: 2024/05/18 21:47:31 by patri            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+static size_t	ft_countwords(const char *s, const char c)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i++] != c && (s[i] == c || s[i] == '\0'))
+			j++;
+	}
+	return (j);
+}
+
+static char	**free_all(char **str, int n)
+{
+	while (n > 0)
+		free(str[n--]);
+	free(str);
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**str;
+	size_t	len;
+	size_t	n;
+
+	str = malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
+	if (!str)
+		return (0);
+	n = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s && *s != c && (int)len++ > -1)
+				s++;
+			str[n] = ft_substr(s - len, 0, len);
+			if (!str[n])
+				return (free_all(str, n));
+			n++;
+		}
+		else
+			s++;
+	}
+	str[n] = NULL;
+	return (str);
+}
+
