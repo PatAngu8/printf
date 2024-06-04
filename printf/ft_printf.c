766df1d57df4ce78cdd4daf2894ddff5bceb09bc
PatAngu8 <123322863+PatAngu8@users.noreply.github.com>
2024-06-04 17:52:42 +0200

    Actualización 04/06
    Creo fundef.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,20 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paangulo <paangulo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paangulo <paangulo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 19:21:02 by paangulo          #+#    #+#             */
-/*   Updated: 2024/05/31 19:52:19 by paangulo         ###   ########.fr       */
+/*   Updated: 2024/06/04 16:53:04 by paangulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
-int	ft_printf(char const *label, ...)
+void	check_type(char a)
+{
+	if ((a == 'c') || (a == '%'))
+		ft_putchar(a);
+	else if (a == 's')
+		ft_putstr(a);
+	else if ((a == 'd') || (a == 'i'))
+		ft_putnbr(a);
+	else if (a == 'u')
+		ft_putnbr_unsigned(a);
+	else if (a == 'p')
+		ft_putptr(a);
+	else if (a == 'x')
+		ft_put_hex_upper(a);
+	else if (a == "X")
+		ft_put_hex_lower(a);		
+}
+int	ft_printf(char const *format, ...)
 {
 	va_list	args;
 	va_list	args_copy;
 
-	va_start(args, label);
+	va_start(args, format);
 	va_copy(args_copy, args);
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			check_type(*format);
+		}		
+	}	
 }
