16459e9eb0e1479be3384059971295a90ab767a3
patri.dev <123322863+PatAngu8@users.noreply.github.com>
2024-06-17 03:04:52 +0200

    printf (en proceso act 17/06 03:04)

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paangulo <paangulo@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: patri <patri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 19:21:02 by paangulo          #+#    #+#             */
-/*   Updated: 2024/06/16 22:44:08 by paangulo         ###   ########.fr       */
+/*   Updated: 2024/06/17 02:54:13 by patri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ static int	ft_check_type(va_list args, char type)
 {
 	if (type == 'c')
 		return (ft_putchar(va_arg(args, int)));
-	else if (type == 's')
+	if (type == 's')
 		return (ft_putstr(va_arg(args, char *)));
-	else if ((type == 'd') || (type == 'i'))
+	if ((type == 'd') || (type == 'i'))
 		return ((ft_putnbr(va_arg(args, int))));
-	else if (type == 'u')
+	if (type == 'u')
 		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
-	else if (type == 'x')
+	if (type == 'x')
 		return (ft_puthex(va_arg(args, unsigned int), 'x'));
-	else if (type == 'X')
+	if (type == 'X')
 		return (ft_puthex(va_arg(args, unsigned int), 'X'));
-	else if (type == 'p')
+	if (type == 'p')
 		return (ft_putptr(va_arg(args, uintptr_t)));
-	else if (type == '%')
+	if (type == '%')
 		return (ft_putchar('%'));
 	return (1);
 }
@@ -37,19 +37,21 @@ int	ft_printf(char const *format, ...)
 {
 	va_list	args;
 	int		len;
+	int		i;
 
 	va_start(args, format);
 	len = 0;
-	while (*format)
+	i = 0;
+	while (format[i] != '\0')
 	{
-		if (*format != '%')
-			len += ft_putchar(*format);
-		if (*format == '%' && *(format + 1))
+		if (format[i] == '%')
 		{
-			len += ft_check_type(args, *format);
-			format++;
+			len += ft_check_type(args, format[i + 1]);
+			i++;
 		}
-		format++;
+		else
+			len += ft_putchar(format[i]);
+		i++;
 	}
 	va_end (args);
 	return (len);
@@ -58,6 +60,29 @@ int	ft_printf(char const *format, ...)
 {
 	unsigned int	num;
 
-	num = 16; 
-	ft_printf("hola\n %x", num);
+	num = 16;
+	ft_printf("hola\n %x", num);*/
+	
+	/*char			*str;
+	char			c;
+	int 	x;
+	void	 *ptr;
+
+	c = 'a';
+	str = "Comprobando ft_printf\n";
+	x = 123;
+	ptr = &x;
+	ft_printf("El caracter es %c\n", c);
+	printf("El caracter es %c\n", c);
+	ft_printf("String: %s\n", str);
+	printf("String: %s\n", str);
+	ft_printf("Puntero: %p\n", ptr);
+	printf("Puntero: %p\n", ptr);
+	ft_printf("Int max: %d, Int min: %i\n", 0, -521);
+	printf("Int max: %d, Int max: %i\n", 0, 521);
+	ft_printf("Unsigned int: %u\n", 4294967295);
+	printf("Unsigned int: %u\n", 429496729);
+	ft_printf("Hexadecimal en minuscula: %x\n", 255);
+	printf("Hexadecimal en minuscula:: %x\n", 255);
+	ft_printf("Hexadecimal en minuscula: %X\n", 255);
 }*/
